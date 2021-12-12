1384de2a1f97ce6ba1c8da9bdd913299a71ab96e
gandhamathan <gandhamathanv.20cse@kongu.edu>
2021-12-12 23:13:15 +0530

    adduser and login clompleted

--- connection.php
+++ connection.php
@@ -3,6 +3,10 @@ $host = 'b9eo15tkau8yyp8hdbg4-mysql.services.clever-cloud.com';
 $dbname = 'b9eo15tkau8yyp8hdbg4';
 $dbusername = 'uu92vcbdst3zdjtv';
 $dbpass = 'EdvSFKXN1P5ZFcS1wuYE';
+// $host = 'localhost';
+// $dbname = 'ominifood';
+// $dbusername = 'root';
+// $dbpass = '';
 $conn = mysqli_connect($host, $dbusername, $dbpass, $dbname);
 if (mysqli_connect_error()) {
     die('connect error(' . mysqli_connect_errno() . ')' . mysqli_connect_error());
