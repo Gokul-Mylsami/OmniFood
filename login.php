41149abe133cfc46d9702c9be4b26d1000efa873
gandhamathan <gandhamathanv.20cse@kongu.edu>
2021-12-15 10:33:14 +0530

    bug released

--- login.php
+++ login.php
@@ -6,9 +6,16 @@ $userPassword = $_POST['pass'];
 $sql = "SELECT password FROM userInformations WHERE email='$userEmail';";
 $res = mysqli_fetch_row(mysqli_query($conn, $sql));
 if (mysqli_error($conn)) {
-    echo mysqli_error($conn);
+    echo "login failed";
 }
 
 if (password_verify($userPassword, $res[0])) {
-    echo "success";
+    echo "connection";
+?>
+<script>
+alert("logined successfully");
+</script>
+<?php
+    sleep(3);
+    header('Location: ' . $_SERVER['HTTP_REFERER']);
 }
---
+++ script.js
@@ -0,0 +1,7 @@
+'use strict';
+
+const docu = document.querySelector('.orderfood-img');
+
+docu.addEventListener('click', function() {
+    console.log("Hi");
+})
